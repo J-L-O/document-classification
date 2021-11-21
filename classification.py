68249a20dbc872d32a51706f31cea0f523b0cb2b
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2021-11-21 20:16:44 +0100

    Start implementing classifier

---
+++ classification.py
@@ -0,0 +1,69 @@
+import os
+from pathlib import Path
+
+import torch
+import celery
+import sys
+
+sys.path.append("./unsupervised_classification")
+from utils.common_config import get_model, get_val_transformations
+from utils.config import create_config
+
+
+class ClassificationTask(celery.Task):
+
+    def __init__(self):
+        sys.path.append(str(Path(__file__).resolve().parent))
+        self.config = {
+            'model_config_path': os.environ.get('CLASSIFICATION_CONFIG_PATH', None),
+            'device_id': int(os.environ.get('CLASSIFICATION_DEVICE', -1))
+        }
+        print(self.config)
+        assert self.config['model_config_path'] is not None, "You must supply a path to a model configuration in the " \
+                                                             "environment variable CLASSIFICATION_CONFIG_PATH "
+
+        self.idx_to_label_map = {
+            0: "0",
+            1: "1"
+        }
+
+        self.document_classifier = None
+        self.transforms = None
+
+    def initialize(self):
+        if self.document_classifier is not None:
+            return
+
+        config_path = Path(self.config['model_config_path'])
+        config_env = config_path / "env.yml"
+        config_exp = config_path / "env.yml"
+        tb_run = ""  # Not needed
+        model_checkpoint = config_path / "model.pth.tar"
+
+        p = create_config(config_env, config_exp, tb_run, make_dirs=False)
+        torch.backends.cudnn.benchmark = True
+
+        model = get_model(p)
+        model = torch.nn.DataParallel(model)
+        model = model.cuda()
+        model.eval()
+
+        checkpoint = torch.load(model_checkpoint, map_location='cpu')
+        model.load_state_dict(checkpoint['model'])
+
+        self.transforms = get_val_transformations(p)
+        self.document_classifier = model
+
+    def predict(self, image):
+
+        transformed_image = self.transforms(image)
+        probs = self.document_classifier(transformed_image)
+
+        predicted_class = int(torch.argmax(probs))
+
+        result = {
+            "predicted_class": self.idx_to_label_map[predicted_class],
+            "confidence": float(probs[predicted_class])
+        }
+
+        return result
--- tasks.py
+++ tasks.py
@@ -1,19 +1,11 @@
 import msgpack
 import os
-import sys
 from io import BytesIO
-from pathlib import Path
 
-import celery
 from PIL import Image
 from celery import Celery
 
-
-class ClassificationTask(celery.Task):
-
-    def __init__(self):
-        pass
-
+from classification import ClassificationTask
 
 broker_address = os.environ.get('BROKER_ADDRESS', 'localhost')
 app = Celery('wpi_demo', backend='rpc://', broker=f"pyamqp://guest@{broker_address}//")
@@ -27,15 +19,22 @@ app.conf.update(
 @app.task(name='document_classification', base=ClassificationTask)
 def classify(task_data):
     bytes = msgpack.unpackb(task_data)
+    classify.initialize()
 
     image_data = BytesIO(bytes)
     image_data.seek(0)
 
+    # print("Before trace")
+    # import pydevd_pycharm
+    # pydevd_pycharm.settrace('192.168.2.104', port=6899, stdoutToServer=True, stderrToServer=True)
+    # print("After trace")
+
     with Image.open(image_data) as decoded_image:
         decoded_image = decoded_image.convert('RGB')
-        classification_result = {
-            "predicted_class": "test",
-            "confidence": 1.0
-        }
+        classification_result = classify.predict(decoded_image)
+        # classification_result = {
+        #     "predicted_class": "test",
+        #     "confidence": 1.0
+        # }
 
     return classification_result
