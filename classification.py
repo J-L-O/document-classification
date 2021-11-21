bed5ba5dcff57deaad885002a2673e976998e881
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2021-11-22 00:46:14 +0100

    Fix classification bugs

--- classification.py
+++ classification.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 
 import torch
+from torchvision import transforms
 import celery
 import sys
 
@@ -23,8 +24,13 @@ class ClassificationTask(celery.Task):
                                                              "environment variable CLASSIFICATION_CONFIG_PATH "
 
         self.idx_to_label_map = {
-            0: "0",
-            1: "1"
+            0: "Auction catalogue",
+            1: "Book of Hours",
+            2: "Book",
+            3: "Legal",
+            4: "Newspaper",
+            5: "Other",
+            6: "Photography"
         }
 
         self.document_classifier = None
@@ -36,7 +42,7 @@ class ClassificationTask(celery.Task):
 
         config_path = Path(self.config['model_config_path'])
         config_env = config_path / "env.yml"
-        config_exp = config_path / "env.yml"
+        config_exp = config_path / "config.yml"
         tb_run = ""  # Not needed
         model_checkpoint = config_path / "model.pth.tar"
 
@@ -44,26 +50,26 @@ class ClassificationTask(celery.Task):
         torch.backends.cudnn.benchmark = True
 
         model = get_model(p)
+        checkpoint = torch.load(model_checkpoint, map_location='cpu')
+        model.load_state_dict(checkpoint['model'])
+
         model = torch.nn.DataParallel(model)
         model = model.cuda()
         model.eval()
 
-        checkpoint = torch.load(model_checkpoint, map_location='cpu')
-        model.load_state_dict(checkpoint['model'])
-
-        self.transforms = get_val_transformations(p)
+        self.transforms = transforms.Compose([transforms.Resize(240), get_val_transformations(p)])
         self.document_classifier = model
 
     def predict(self, image):
-
         transformed_image = self.transforms(image)
-        probs = self.document_classifier(transformed_image)
+        probs = self.document_classifier(transformed_image.unsqueeze(0))
+        probs = probs[0].squeeze()
 
-        predicted_class = int(torch.argmax(probs))
+        predicted_class = torch.argmax(probs).item()
 
         result = {
             "predicted_class": self.idx_to_label_map[predicted_class],
-            "confidence": float(probs[predicted_class])
+            "confidence": probs[predicted_class].item()
         }
 
         return result
